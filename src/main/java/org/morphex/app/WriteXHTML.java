efc59b4445ed354856b60cd322902913043dc973
Morten W. Petersen <morphex@gmail.com>
2018-04-03 12:37:54 +0200

    Testing out writing of XHTML UTF file.

--- src/main/java/org/morphex/app/WriteXHTML.java
+++ src/main/java/org/morphex/app/WriteXHTML.java
@@ -1,13 +1,16 @@
 import java.io.*;
+import java.nio.charset.Charset;
 
 public class WriteXHTML {
-	static String header = "<?xml version='1.0' encoding='utf-16' ?><!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'><html xmlns='http://www.w3.org/1999/xhtml'><head><title>Test</title></head><body>";
-	static String footer = "</body></html>";
+	static Charset utf = Charset.forName("UTF-8");
+	static String header = new String("<?xml version='1.0' encoding='utf-8' ?><!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'><html xmlns='http://www.w3.org/1999/xhtml'><head><title>Test</title></head><body>");
+	static String footer = new String("</body></html>");
 
 	public static void main(String[] args)  throws IOException {
 		Writer out = new OutputStreamWriter(new FileOutputStream(
-			"test.html"), "UTF-16");
+			"test.html"), "UTF-8");
 		out.write(header);
+		String test = new String("This is a test");
 		out.write("This is a test");
 		out.write(footer);
 		out.close();
