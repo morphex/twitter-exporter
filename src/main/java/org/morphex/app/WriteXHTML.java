f8fca4445f556099412094fa37efe485b1328079
Morten W. Petersen <morphex@gmail.com>
2018-04-03 13:51:58 +0200

    Committing

--- src/main/java/org/morphex/app/WriteXHTML.java
+++ src/main/java/org/morphex/app/WriteXHTML.java
@@ -2,17 +2,21 @@ import java.io.*;
 import java.nio.charset.Charset;
 
 public class WriteXHTML {
-	static Charset utf = Charset.forName("UTF-8");
-	static String header = new String("<?xml version='1.0' encoding='utf-8' ?><!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'><html xmlns='http://www.w3.org/1999/xhtml'><head><title>Test</title></head><body>");
+	static String charset_ = "UTF-16LE";
+	// static Charset utf = Charset.forName(charset);
+	static String header = new String("<?xml version='1.0' encoding='" + charset_ + "' ?><!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'><html xmlns='http://www.w3.org/1999/xhtml'><head><title>Test</title></head><body>");
 	static String footer = new String("</body></html>");
 
-	public static void main(String[] args)  throws IOException {
-		Writer out = new OutputStreamWriter(new FileOutputStream(
-			"test.html"), "UTF-8");
-		out.write(header);
+	public static byte[] e(String toEncode) throws Exception {
+		return toEncode.getBytes(charset_);
+	}
+
+	public static void main(String[] args)  throws Exception {
+		FileOutputStream out = new FileOutputStream("test.html");
+		out.write(e(header));
 		String test = new String("This is a test");
-		out.write("This is a test");
-		out.write(footer);
+		out.write(e("This is a test"));
+		out.write(e(footer));
 		out.close();
 	}
 }
