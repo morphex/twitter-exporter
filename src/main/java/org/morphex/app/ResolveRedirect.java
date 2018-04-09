0e2345e507c6942658ef3a1757cbf4f577cd66c1
Morten W. Petersen <morphex@gmail.com>
2018-04-09 08:25:11 +0200

    Refactoring, enabling resolvement of shortened URLs

--- src/main/java/org/morphex/app/ResolveRedirect.java
+++ src/main/java/org/morphex/app/ResolveRedirect.java
@@ -1,26 +1,59 @@
 import javax.net.ssl.*;
 import java.io.*;
 
-public class ResolveRedirect {
-	public static void main(String[] args) throws Exception {
+class SSLConnection {
+	SSLSocket socket;
+	PrintWriter send;
+	BufferedReader receive;
+	String host;
+
+	public SSLConnection(String host_) throws Exception {
 		SSLSocketFactory factory = (SSLSocketFactory)SSLSocketFactory.getDefault();
-		SSLSocket socket = (SSLSocket)factory.createSocket("t.co", 443);
+		SSLSocket socket = (SSLSocket)factory.createSocket(host_, 443);
 		socket.startHandshake();
-		PrintWriter send = new PrintWriter(new BufferedWriter(
+		host = host_;
+		send = new PrintWriter(new BufferedWriter(
 			new OutputStreamWriter(socket.getOutputStream())));
-		send.println("GET /TQuFITDB4r HTTP/1.0");
-		send.println("Host: t.co:443");
+		receive = new BufferedReader(
+			new InputStreamReader(socket.getInputStream()));
+	}
+
+	String getRedirect(String path) throws IOException {
+		send.println("GET " + path + " HTTP/1.1");
+		send.println("Host: " + host);
 		send.println();
 		send.flush();
-		BufferedReader receive = new BufferedReader(
-			new InputStreamReader(socket.getInputStream()));
 		String line = "";
+		String location = "";
 		do {
-			System.out.println(line);
+			// System.out.println(line);
+			// System.out.println("Before readLine..");
 			line = receive.readLine();
-		} while (line != null);
-		send.close();
-		receive.close();
-		socket.close();
+			if (line.toLowerCase().startsWith("location:")) {
+				location = line.split("\\s")[1];
+			}
+		} while (!line.trim().isEmpty());
+		return location;
+	}
+
+	protected void finalize() throws Throwable {
+		try {
+			send.close();
+			receive.close();
+			socket.close();
+		} finally {
+			super.finalize();
+		}
+
+	}
+}
+
+public class ResolveRedirect {
+
+	public static void main(String[] args) throws Exception {
+		SSLConnection connection = new SSLConnection("t.co");
+		System.out.println("Lo: " +
+			connection.getRedirect("/MdN9Zjwo43"));
+		connection = null;
 	}
 }
